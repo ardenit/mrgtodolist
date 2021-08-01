66547cce286947038496133da337b83d80e24f65
ardenit <zaitsev.eo@gmail.com>
2021-08-01 07:16:49 +0300

    Changed FAB color

--- app/src/main/java/com/mirage/todolist/TodolistActivity.kt
+++ app/src/main/java/com/mirage/todolist/TodolistActivity.kt
@@ -1,14 +1,19 @@
 package com.mirage.todolist
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.Menu
-import android.view.MenuItem
 import android.widget.FrameLayout
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.viewpager.widget.ViewPager
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.mikepenz.materialdrawer.model.DividerDrawerItem
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem
@@ -16,7 +21,8 @@ import com.mikepenz.materialdrawer.model.interfaces.iconRes
 import com.mikepenz.materialdrawer.model.interfaces.nameRes
 import com.mikepenz.materialdrawer.util.addItems
 import com.mikepenz.materialdrawer.widget.MaterialDrawerSliderView
-import com.mirage.todolist.ui.main.SectionsPagerAdapter
+import com.mirage.todolist.ui.main.PlaceholderFragment
+
 
 class TodolistActivity : AppCompatActivity() {
 
@@ -38,11 +44,41 @@ class TodolistActivity : AppCompatActivity() {
         }
         drawerSlider = findViewById(R.id.act_main_nav_slider)
 
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = findViewById(R.id.view_pager)
-        viewPager.adapter = sectionsPagerAdapter
+        val viewPager: ViewPager2 = findViewById(R.id.view_pager)
+        viewPager.offscreenPageLimit = 3
+        viewPager.adapter = object : FragmentStateAdapter(supportFragmentManager, lifecycle) {
+            override fun getItemCount(): Int {
+                return 3
+            }
+
+            override fun createFragment(position: Int): Fragment {
+                return PlaceholderFragment.newInstance(position + 1)
+            }
+
+        }
+        val tabTitles = arrayOf(
+            R.string.footer_archive_btn,
+            R.string.footer_todo_btn,
+            R.string.footer_done_btn
+        )
+        val tabIcons = arrayOf(
+            R.drawable.ic_nav_archive,
+            R.drawable.ic_nav_todo,
+            R.drawable.ic_nav_done
+        )
         val tabs: TabLayout = findViewById(R.id.tabs)
-        tabs.setupWithViewPager(viewPager)
+        tabs.setSelectedTabIndicatorColor(ContextCompat.getColor(this, R.color.light_orange))
+        tabs.setTabTextColors(ContextCompat.getColor(this, R.color.light_grey),
+            ContextCompat.getColor(this, R.color.light_orange))
+        TabLayoutMediator(tabs, viewPager, true, true) { tab, position ->
+            tab.setText(tabTitles[position])
+            tab.setIcon(tabIcons[position])
+            tab.icon?.let {
+                val colorStateList = ContextCompat.getColorStateList(this, R.color.todolist_footer_btn_color)
+                val coloredIcon = DrawableCompat.wrap(it)
+                DrawableCompat.setTintList(coloredIcon, colorStateList)
+            }
+        }.attach()
     }
 
     override fun onBackPressed() {
