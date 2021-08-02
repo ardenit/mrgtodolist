92e207734aa167cbe0e91b7d5bf3f4ddb1ec878a
ardenit <zaitsev.eo@gmail.com>
2021-08-02 06:46:31 +0300

    Added task item layout

---
+++ app/src/main/java/com/mirage/todolist/content/TasklistRecyclerAdapter.kt
@@ -0,0 +1,12 @@
+package com.mirage.todolist.content
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class TasklistRecyclerAdapter {
+
+    inner class TasklistViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+}
