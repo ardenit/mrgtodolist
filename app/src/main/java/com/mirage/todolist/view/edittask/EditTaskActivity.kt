22638432875b24713d60b197790c96571f34e839
ardenit <zaitsev.eo@gmail.com>
2021-08-27 17:15:25 +0300

    Added close confirm dialog to editor screen

--- app/src/main/java/com/mirage/todolist/view/edittask/EditTaskActivity.kt
+++ app/src/main/java/com/mirage/todolist/view/edittask/EditTaskActivity.kt
@@ -7,7 +7,10 @@ import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.ContextThemeWrapper
+import androidx.core.content.ContextCompat
 import com.mirage.todolist.R
 import com.mirage.todolist.model.tasks.LiveTag
 import com.mirage.todolist.model.tasks.LiveTask
@@ -25,6 +28,8 @@ class EditTaskActivity : AppCompatActivity() {
     private var initialTask: LiveTask? = null
     private var tasklistID: Int = 1
     private var newTagsList: MutableList<LiveTag> = arrayListOf()
+    /** Flag to prevent multiple clicks on "Save" button */
+    private var savePressed: Boolean = false
 
     private lateinit var titleInput: EditText
     private lateinit var descriptionInput: EditText
@@ -74,6 +79,8 @@ class EditTaskActivity : AppCompatActivity() {
     }
 
     private fun saveTask() {
+        if (savePressed) return
+        savePressed = true
         val newTitle = titleInput.text.toString()
         val newDescription = descriptionInput.text.toString()
         val task = initialTask ?: todolistModel.createNewTask(tasklistID)
@@ -87,9 +94,42 @@ class EditTaskActivity : AppCompatActivity() {
         super.onBackPressed()
     }
 
+    private fun openBackConfirmDialog() {
+        val alertDialog = AlertDialog.Builder(ContextThemeWrapper(this, R.style.Theme_TodoApp_EditTaskAlertDialog))
+            .setTitle(R.string.edit_task_back_confirm_title)
+            .setMessage(R.string.edit_task_back_confirm_description)
+            .setPositiveButton(R.string.edit_task_back_save_btn) { _, _ ->
+                saveTask()
+            }
+            .setNegativeButton(R.string.edit_task_back_discard_btn) { _, _ ->
+                super.onBackPressed()
+            }
+            .setNeutralButton(R.string.edit_task_back_cancel_btn) { _, _ -> }
+            .create()
+        val positiveColor = ContextCompat.getColor(this, R.color.light_blue)
+        val negativeColor = ContextCompat.getColor(this, R.color.light_orange)
+        val neutralColor = ContextCompat.getColor(this, R.color.light_grey)
+        alertDialog.show()
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(positiveColor)
+        alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(negativeColor)
+        alertDialog.getButton(AlertDialog.BUTTON_NEUTRAL).setTextColor(neutralColor)
+    }
+
     override fun onBackPressed() {
-        //TODO if changed, confirm dialog
-        super.onBackPressed()
+        val newTitle = titleInput.text.toString()
+        val newDescription = descriptionInput.text.toString()
+        val task = initialTask ?: todolistModel.createNewTask(tasklistID)
+        //TODO other changes
+        if (
+            newTitle != task.title.value ||
+            newDescription != task.description.value ||
+            newTagsList != task.tags.value
+        ) {
+            openBackConfirmDialog()
+        }
+        else {
+            super.onBackPressed()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
